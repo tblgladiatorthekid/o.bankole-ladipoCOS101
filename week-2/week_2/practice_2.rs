4139ec83f4d4f66137d1d9b70a4d2a50c5004985
Oluwatobiloba Bankole-Ladipo <bankoleladipotobi@gmail.com>
2024-10-23 06:31:00 +0100

    My Rust projects

---
+++ week-2/week_2/practice_2.rs
@@ -0,0 +1,11 @@
+fn main() {
+	println!();
+
+	println!("hello");
+
+	println!("format {} arguments", "some");
+
+	println!("My name is {}. Nice to meet you", "Oluwatobiloba Bankole-Ladipo");
+
+
+}
